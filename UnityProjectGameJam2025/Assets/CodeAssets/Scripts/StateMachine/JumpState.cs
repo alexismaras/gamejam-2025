23fe58ff354580013759fe6cc251b7657978eea4
musa <musa.k123@outlook.de>
2025-07-08 12:47:40 +0200

    GameCode(Jump) : Bug fixxes
    Co-Authored-By: alexismaras <153950827+alexismaras@users.noreply.github.com>

--- UnityProjectGameJam2025/Assets/CodeAssets/Scripts/StateMachine/JumpState.cs
+++ UnityProjectGameJam2025/Assets/CodeAssets/Scripts/StateMachine/JumpState.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.UIElements;
 
 
 public class JumpState : IState
@@ -10,6 +11,9 @@ public class JumpState : IState
 
     private float _elapsedTimeSinceJump;
     private float _airTime;
+
+    private float _startHeight;
+
     
     public JumpState(PlayerController player)
     {
@@ -23,14 +27,14 @@ public class JumpState : IState
         _elapsedTimeSinceJump = 0;
         _airTime = 0;
         player.PlayerRigidbody.useGravity = false;
+        _startHeight = player.PlayerRigidbody.position.y;
     }
 
     // Update is called once per frame
     public void Update()
     {
         _elapsedTimeSinceJump += Time.deltaTime;
-
-        if (player.PlayerRigidbody.position.y >= player.JumpMaxHeight)
+        if (player.PlayerRigidbody.position.y >= _startHeight + player.JumpMaxHeight)
         {
             _airTime += Time.deltaTime;
 
