0eaf66297bebea7b41b508a0c5fc13aad03b5807
musa <musa.k123@outlook.de>
2025-07-08 11:48:16 +0200

    GameCode(SpawnManager + Cube Logic): Adjusted Base SpawnManager Logic + Added Cube Logic

--- UnityProjectGameJam2025/Assets/GameDesignOrdner/Script/IceCubeScript.cs
+++ UnityProjectGameJam2025/Assets/GameDesignOrdner/Script/IceCubeScript.cs
@@ -1,18 +1,52 @@
+using NUnit.Framework;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class IceCubeScript : MonoBehaviour
 {
+
+    //[SerializeField] private Transform[] _littleBlockSpawn;
+    [SerializeField] private GameObject _littleBlock;
+
+    [SerializeField] private List<Transform> _littleBlockSpawn = new List<Transform>();
+
+    private List<Transform> _littleBlockSpawnLaufzeit = new List<Transform>();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         StartCoroutine("_selfDestroy");
+
+        _littleBlockSpawnLaufzeit = _littleBlockSpawn;
+
+        _spawnLittleCubes();
     }
 
     private IEnumerator _selfDestroy()
     {
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(5);
         SpawnManagerScript.Instance.SpawnObject();
         Destroy(gameObject);
+    }
+
+    private void _spawnLittleCubes()
+    {
+        while (_littleBlockSpawnLaufzeit.Count > 3)
+        {
+            int randomIndex = Random.Range(0, _littleBlockSpawnLaufzeit.Count);
+
+            // Übernehme den Spawnpoint von der Liste
+            Transform CurrentSpawnPoint = _littleBlockSpawnLaufzeit[randomIndex];
+
+            // Kleinen Cube auf pos und rotation 0 instanziieren und Random Zahl würfeln
+            GameObject instance = Instantiate(_littleBlock, CurrentSpawnPoint.transform.position, Quaternion.identity);
+
+            // Binde den kleinen Cube an den SpawnPoint
+            instance.transform.parent = CurrentSpawnPoint;
+
+            // Entferne ihn aus der Laufzeit Liste, damit er nicht am gleichen Punkt wieder Spawnen kann
+            _littleBlockSpawnLaufzeit.RemoveAt(randomIndex);
+        }
     }
 }
--- UnityProjectGameJam2025/Assets/GameDesignOrdner/Script/SpawnManagerScript.cs
+++ UnityProjectGameJam2025/Assets/GameDesignOrdner/Script/SpawnManagerScript.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SpawnManagerScript : MonoBehaviour
 {
 
     public static SpawnManagerScript Instance;
 
-    [SerializeField] private GameObject[] _spawnPoint;
+    [SerializeField] private List<GameObject> _spawnPoints = new List<GameObject>();
+    private GameObject _placeHolder;
 
     [SerializeField] private GameObject _objectToSpawn;
 
@@ -20,8 +22,27 @@ public class SpawnManagerScript : MonoBehaviour
 
     public void SpawnObject()
     {
-        Debug.Log("RUF SPAWN OBJECT!");
-        int RandomIndex = Random.Range(0, _spawnPoint.Length);
-        GameObject instance = Instantiate(_objectToSpawn, _spawnPoint[RandomIndex].transform.position, Quaternion.identity);
+        int RandomIndex = Random.Range(0, _spawnPoints.Count);
+
+        if (_placeHolder != _spawnPoints[RandomIndex])
+        {
+            _placeHolder = _spawnPoints[RandomIndex];
+            _spawnPoints.Add(_placeHolder);
+            Debug.Log("Neuer Cube gespawned!");
+            GameObject instance = Instantiate(_objectToSpawn, _spawnPoints[RandomIndex].transform.position, Quaternion.identity);
+            _spawnPoints.RemoveAt(RandomIndex);
+        }
+        else if (_placeHolder == _spawnPoints[RandomIndex])
+        {
+            SpawnObject();
+            return;
+        }
+        else if (_placeHolder == null)
+        {
+            _placeHolder = _spawnPoints[RandomIndex];
+            Debug.Log("Neuer Cube gespawned!");
+            GameObject instance = Instantiate(_objectToSpawn, _spawnPoints[RandomIndex].transform.position, Quaternion.identity);
+            _spawnPoints.RemoveAt(RandomIndex);
+        }
     }
 }
